cc42df9ca32edcd1d1c41f3f57cf2d72bc0025fa
ishavaikar29-prog <ishavaikar29@gmail.com>
2025-11-28 17:00:59 +0530

    Update test_local_api.py
    url change , body added , method changed to post

--- test_local_api.py
+++ test_local_api.py
@@ -2,16 +2,29 @@ import requests
 import json
 import time
 
-API_URL = " https://dev.app.creditos.in/api/v1/verifyDomain"
+API_URL = "http://192.168.200.215:3000/api/v1/verifyDomain"   # change to your endpoint
 
-# Wait for API to boot up
-time.sleep(3)
+# JSON body you want to send
+API_Body: 
+{
+
+    "bankDomainName": "testbankt1"
+
+}
+
+
+time.sleep(3)  # wait for local API to start
 
 try:
-    response = requests.get(API_URL)
+    print(f"Sending POST request to: {API_URL}")
+    print(f"API_Body: {API_Body}")
+
+    response = requests.post(API_URL, json=API_Body)
     response.raise_for_status()
+
     data = response.json()
 
+    # Save API response in artifact file
     with open("local_api_output.json", "w") as f:
         json.dump(data, f, indent=4)
 
